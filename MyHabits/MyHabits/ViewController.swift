9d4370e211bcb7823dace806a248565d24d5ecd5
Filatov_Yurii <filatov.ios.dev@gmail.com>
2022-04-15 21:29:50 +0300

    Start prod

---
+++ MyHabits/MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Юрий Филатов on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
